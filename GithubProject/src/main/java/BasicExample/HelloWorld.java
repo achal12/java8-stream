3f1cccc23f3d33e8f2dfcc6af9624f33fe1af3de
achal12 <68753421+achal12@users.noreply.github.com>
2022-04-21 10:51:12 +0530

    Basic code

---
+++ GithubProject/src/main/java/BasicExample/HelloWorld.java
@@ -0,0 +1,8 @@
+package BasicExample;
+
+public class HelloWorld{
+	   public static void main(String[] args) {
+	        System.out.println("Hello World");
+	        System.out.println("Welcome to java 8");
+	    }
+	 }
